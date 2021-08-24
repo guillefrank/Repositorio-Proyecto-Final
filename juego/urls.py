3bc86a769c639c104f4ca44ca746f76180812690
LeosuiaXD <LeosuiaXD@gmail.com>
2021-08-24 02:31:07 -0300

    creamos una url con vista y un archivo html=Todo OK

---
+++ juego/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.listar_preguntas, name='listar_preguntas'),
+]
--- juego/views.py
+++ juego/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
 # Create your views here.
+
+def listar_preguntas(request):
+    return render(request, 'juego/listar_preguntas.html', {})
